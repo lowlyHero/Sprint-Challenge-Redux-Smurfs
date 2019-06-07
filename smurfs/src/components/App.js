5e19717694fca3c035c3987daee4c4a8c92a06cb
lowlyHero <e.nunn1009@gmail.com>
2019-06-07 13:26:11 -0400

    Got Brainey to render on screen

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { getSmurfs } from '../actions';
 
 import './App.css';
 
-const App = props => {
-  const fetchSmurf = e => {
-    e.preventDefault();
-    props.getSmurfs();
-  };
+class App extends Component {
 
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
+  render() {
     return (
       <div className='App'>
       <h1>Smurfs!</h1>
-      {props.smurfs && (
-        <div>
-          <p>{props.smurfs.name}</p>
-          <p>{props.smurfs.height}</p>
-          <p>{props.smurfs.age}</p>
-       </div>
-      )}
- 
-    <button onClick={fetchSmurf}>Get Smurfs</button>
+      <span>{this.props.smurfs.map(smurf => {
+        return (
+          <div>
+          <li>{smurf.name}</li>
+          <li>{smurf.age}</li>
+          <li>{smurf.height}</li>
+          </div>
+        )
+      })}</span>
     </div>
     );
-};
+  }
+}
+
 
 const mapStateToProps = state => ({
   smurfs: state.smurfs,
